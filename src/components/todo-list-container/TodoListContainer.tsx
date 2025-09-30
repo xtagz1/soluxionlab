95027422df3c7da063baa8931f0a0927e21cf48e
jonixx <aljonkarlusmartinez48@gmail.com>
2025-09-30 14:37:45 +0800

    chore: udpate conditions for existing container

--- src/components/todo-list-container/TodoListContainer.tsx
+++ src/components/todo-list-container/TodoListContainer.tsx
@@ -1,10 +1,10 @@
 import ExistingtaskContainer from "./ExistingtaskContainer";
 import NewTaskField from "./NewTaskField";
 
-export default function TodoListContainer() {
+export default function TodoListContainer({ isEditing }: any) {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-            <NewTaskField />
+            {isEditing && <NewTaskField />}
             <ExistingtaskContainer />
         </div>
     )
--- src/components/todo-main-container/TodoMainContainer.tsx
+++ src/components/todo-main-container/TodoMainContainer.tsx
@@ -10,7 +10,7 @@ export default function TodoContainer() {
         <div style={{ backgroundImage: '' }}>
             <div style={{ marginTop: '2%', marginBottom: '2%', marginLeft: '10%', marginRight: '10%', display: 'flex', flexDirection: 'column', gap: '20px' }}>
                 <Header isEditing={isEditing} />
-                <TodoListContainer />
+                <TodoListContainer isEditing={isEditing} />
                 <FooterActions setEditing={setEditing} isEditing={isEditing} />
             </div>
         </div>
